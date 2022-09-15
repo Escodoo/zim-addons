65d43cd761dbe5d3e83307ffa1d426c224467b41
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-09-15 10:32:43 -0300

    [ADD] zim_sale_custom module

---
+++ zim_sale_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Zim Sale Custom",
+    "summary": """
+        ZIM Sale Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/zim-addons",
+    "depends": ["l10n_br_sale"],
+    "data": [
+        "views/sale_report_templates.xml",
+        "views/sale_portal_templates.xml",
+    ],
+    "demo": [],
+}
---
+++ zim_sale_custom/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+* add freight_value on quotation/sale order document
+* add freight_value on sale order portal (by product)
